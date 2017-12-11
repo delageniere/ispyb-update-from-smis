4449728b21a437da21e74601843c83a8a20fd207
delageniere <delageni@esrf.fr>
2017-12-11 11:40:29 +0100

    commits

--- src/main/java/webservice/batchUpdate/BatchUpdateFromSMIS.java
+++ src/main/java/webservice/batchUpdate/BatchUpdateFromSMIS.java
@@ -6,6 +6,7 @@ import javax.xml.ws.BindingProvider;
 
 import generated.ws.common.update.IspybWS;
 import generated.ws.common.update.UpdateFromSMISWebService;
+import webservice.UtilsDoNotCommit;
 
 /**
  * 
